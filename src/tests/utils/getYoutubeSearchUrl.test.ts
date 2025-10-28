0972a6177fb73c354eee91357282b14b53bb3f1b
Eduardo Ferras <edferras@gmail.com>
2025-10-28 16:32:20 -0300

    test: add test for getYoutubeSearchUrl util

---
+++ src/tests/utils/getYoutubeSearchUrl.test.ts
@@ -0,0 +1,28 @@
+import getYoutubeSearchUrl from '@/utils/getYoutubeSearchUrl'
+import { describe, it, expect } from 'vitest'
+
+describe('getYoutubeSearchUrl', () => {
+	it('should return correct YouTube search URL for a simple query', () => {
+		const query = 'test video'
+		const expectedUrl = 'https://www.youtube.com/results?search_query=test%20video'
+		expect(getYoutubeSearchUrl(query)).toBe(expectedUrl)
+	})
+
+	it('should handle special characters in the query', () => {
+		const query = 'C++ programming'
+		const expectedUrl = 'https://www.youtube.com/results?search_query=C%2B%2B%20programming'
+		expect(getYoutubeSearchUrl(query)).toBe(expectedUrl)
+	})
+
+	it('should handle queries with symbols', () => {
+		const query = 'rock & roll'
+		const expectedUrl = 'https://www.youtube.com/results?search_query=rock%20%26%20roll'
+		expect(getYoutubeSearchUrl(query)).toBe(expectedUrl)
+	})
+
+	it('should handle queries with non-ASCII characters', () => {
+		const query = '你好'
+		const expectedUrl = 'https://www.youtube.com/results?search_query=%E4%BD%A0%E5%A5%BD'
+		expect(getYoutubeSearchUrl(query)).toBe(expectedUrl)
+	})
+})
