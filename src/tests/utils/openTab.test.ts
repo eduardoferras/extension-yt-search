1a5298d9d483b3b92d963ed9f308a923b292e9f5
Eduardo Ferras <edferras@gmail.com>
2025-10-28 16:31:29 -0300

    test: add test for openTab

---
+++ src/tests/utils/openTab.test.ts
@@ -0,0 +1,24 @@
+import openTab from '@/utils/openTab'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+describe('openTab', () => {
+	beforeEach(() => {
+		global.chrome = {
+			tabs: {
+				create: vi.fn()
+			}
+		} as unknown as typeof chrome
+	})
+
+	afterEach(() => {
+		vi.clearAllMocks()
+	})
+
+	it('should open a new tab with the given URL', () => {
+		const url = 'https://youtube.com'
+		openTab(url)
+
+		expect(chrome.tabs.create).toHaveBeenCalledWith({ url })
+		expect(chrome.tabs.create).toHaveBeenCalledTimes(1)
+	})
+})
